fbfc19e820f9cd999f17d6c31501049b19e175a2
shangdi1122 <1399070052@qq.com>
2017-08-04 22:17:38 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/xiangmu/First.java
@@ -0,0 +1,14 @@
+package cn.bdqn.tangcco.tcmp056.xiangmu;
+
+/**
+ * Created by THINKPAD on 2017/8/4.
+ */
+public class First {
+
+    public static void main(String[] args) {
+
+        System.out.println("大家好！！");
+
+        System.out.println("谢谢观看");
+    }
+}
---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/xiangmu/Second.java
@@ -0,0 +1,15 @@
+package cn.bdqn.tangcco.tcmp056.xiangmu;
+
+import org.junit.Test;
+
+/**
+ * Created by THINKPAD on 2017/8/4.
+ */
+public class Second {
+
+   @Test
+    public void print() {
+
+       System.out.println("这是调用！ 记住test不能存在！！");
+   }
+}
